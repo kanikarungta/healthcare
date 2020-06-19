1cf435eef4f39419c64853983d75e8eaebb4c070
kanika rungta <kanikarungta96@gmail.com>
2020-06-19 23:37:07 +0530

    app.js file missed in routing working fine

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import React from 'react';
+import './App.css'
 import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom'
-import Doctor from './components/doctor'
-import Patient from './components/patient'
-import Pharmacist from './components/pharmacist'
 import Login from './components/login'
-import './App.css';
-
+import ProtectedRoute from './components/protectedRoute'
 
 function App() {
   return (
     <div className='content'>
       <Router>
         <Switch>
-          <Route path="/doctor" exact component={Doctor} />
-          <Route path="/patient" exact component={Patient} />
-          <Route path="/pharmacist" exact component={Pharmacist} />
+          <ProtectedRoute path="/doctor" exact />
+          <ProtectedRoute path="/patient" exact />
+          <ProtectedRoute path="/pharmacist" exact />
           <Route path="/login" component={Login} />
-          
           <Redirect to="/login"/>
         </Switch>
     </Router>
